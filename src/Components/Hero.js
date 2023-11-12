5fe42728fffe93645290b972c5bcc0efa83b4225
manveira <100451657+manveira@users.noreply.github.com>
2023-11-12 03:04:32 -0500

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -37,19 +37,13 @@ export default function Hero() {
               </span>
             </p>
             <div class="social-links">
-              <a href="https://www.youtube.com/channel/UC8TXgNmZW2hMis074yGOTUQ">
-                <i class="bx bxl-youtube"></i>
-              </a>
-              <a href="https://www.instagram.com/bhushan_borole_/">
-                <i class="bx bxl-instagram"></i>
-              </a>
-              <a href="https://github.com/bhushan-borole">
+              <a href="https://github.com/manveira">
                 <i class="bx bxl-github"></i>
               </a>
-              <a href="https://www.linkedin.com/in/bhushan-borole/">
+              <a href="https://www.linkedin.com/in/j-veira/">
                 <i class="bx bxl-linkedin"></i>
               </a>
-              <a href="mailto:borolebhushan8@gmail.com">
+              <a href="mailto:manveira159@gmail.com">
                 <i class="bx bx-envelope"></i>
               </a>
             </div>
