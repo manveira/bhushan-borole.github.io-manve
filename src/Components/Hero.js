11e7c510a7d1696d9da3e6f6e7410ca56d34a99c
manveira <100451657+manveira@users.noreply.github.com>
2023-11-13 18:51:13 -0500

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -48,6 +48,7 @@ export default function Hero() {
               </a>
             </div>
             <br />
+            <br />        
             <Button
               variant="outlined"
               target="_blank"
